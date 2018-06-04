7ced4d0b4a58aecadc253fc50ae13534ddcb6ff4
gabriellapowell <soccerplayer60@icloud.com>
2018-06-04 21:55:42 +0000

    automatically backed up by learn

--- about_me.rb
+++ about_me.rb
@@ -1,10 +1,9 @@
 def about_me 
 puts "My name is Gabriella."
 puts "I am 15 years old."
-puts "I have a brother, a sister, and a dog."
 puts "I have blue eyes."
 puts "I am 15 years old."
 end
 
-about_me
+def 
 about_me
